8f7c276593dd180858a2d111eadb379f5e2986e4
Daniele Falchetti <danielefalche@gmail.com>
2024-09-13 23:23:26 +0200

    u

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://ago-dan.com",
+  integrations: [sitemap()],
+});
