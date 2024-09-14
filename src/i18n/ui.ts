6d339b26a286cec9029996b3ee9daade07d9482d
Daniele Falchetti <danielefalche@gmail.com>
2024-09-14 16:38:53 +0200

    f

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,6 +14,7 @@ export const ui = {
     photo: "Photos",
     celebration: "Celebration",
     hourCelebration: "6:30 pm",
+    ride: "Need a ride to the wedding venue?\n\nThere will be a bus waiting for you here at 5:30 pm!\nIt will bring you back to this other location\nonce the party is over at around 3 am.",
   },
   es: {
     hello: "Hola!",
@@ -23,6 +24,7 @@ export const ui = {
     photo: "Fotos",
     celebration: "Boda",
     hourCelebration: "18.30",
+    ride: "¿No tenés cómo ir al salón de fiestas?\n\nVa a haber un colectivo esperándote acá a las 17:30!\nTe traerá de vuelta a esta otra ubicación\nuna vez que termine la fiesta a las 3 am aprox.",
   },
   it: {
     hello: "Ciao!",
@@ -32,5 +34,6 @@ export const ui = {
     photo: "Foto",
     celebration: "Ricevimento",
     hourCelebration: "18.30",
+    ride: "Non hai come andare al ricevimento?\n\nCi sarà un autobus ad aspettarti qui alle 17:30!\nTi riporterà a quest'altro indirizzo\nuna volta terminata la festa, intorno alle 3 del mattino.",
   },
 } as const;
